4c8799b95f8e742bb07ca2843f93080109c68f1e
Alba <albadvg@yahoo.es>
2022-09-03 18:02:06 +0200

    base de trabajo

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//variables
+var pestanaIniciarSesion = document.querySelector('.pestana-iniciar-sesion');
+var pestanaCrearCuenta = document.querySelector('.pestana-crear-cuenta');
+var formularioIniciarSesion = document.querySelector('#iniciar-sesion');
+var formularioCrearCuenta = document.querySelector('#crear-cuenta')
+
+console.log(pestanaIniciarSesion)
+console.log(formularioIniciarSesion)
+console.log(formularioCrearCuenta)
+
+//event listeners
+pestanaIniciarSesion.addEventListener('click', cambiarPestana)
+pestanaCrearCuenta.addEventListener('click', cambiarPestana)
+
+
+//funciones
+/* function cambiarPestaña() {
+    if (this.classList.contains('pestaña-inactiva')){
+        this.classList.remove('pestaña-inactiva');
+        this.
+        if(formularioIniciarSesion.classList.contains('hidden')) {
+            formularioIniciarSesion.classList.remove('hidden');
+            formularioCrearCuenta.classList.add('hidden')
+        } else {
+            formularioCrearCuenta.classList.remove('hidden');
+            formularioIniciarSesion.classList.add('hidden')
+        }
+    } else {
+        return;
+    }
+} */
+
+function cambiarPestana()  {
+    if (this === pestanaIniciarSesion && this.classList.contains('pestana-inactiva')){
+        this.classList.remove('pestana-inactiva');
+        pestanaCrearCuenta.classList.add('pestana-inactiva')
+        formularioIniciarSesion.classList.remove('hidden');
+        formularioCrearCuenta.classList.add('hidden');
+    } else if (this === pestanaCrearCuenta && this.classList.contains('pestana-inactiva')) {
+        this.classList.remove('pestana-inactiva');
+        pestanaIniciarSesion.classList.add('pestana-inactiva')
+        formularioCrearCuenta.classList.remove('hidden');
+        formularioIniciarSesion.classList.add('hidden');
+    }
+}
